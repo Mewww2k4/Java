6e0ef789fe6150aa5740f56f52f47cd23015b263
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-09-27 23:02:49 +0700

    update

--- OOP/OOP/Class/Student.java
+++ OOP/OOP/Class/Student.java
@@ -1,4 +1,5 @@
 package OOP.Class;
+import java.util.Scanner;
 
 public class Student {
     String name;
@@ -6,9 +7,14 @@ public class Student {
     String address;
 
     public Student(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+
+    public Student()
+    {
+
     }
 
     public void setName(String name) {
@@ -32,7 +38,28 @@ public class Student {
     }
 
     public String getAddress() {
-        return this.address;
+      return this.address;
+    }
+
+    public void Input()
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Nhap ho va ten : ");
+      String name = sc.nextLine();
+      System.out.print("Nhap tuoi: ");
+      int age = sc.nextInt();
+      sc.nextLine();
+      System.out.print("Nhap dia chi: ");
+      String address = sc.nextLine();
+      //set
+      this.setName(name);
+      this.setAge(age);
+      this.setAddress(address);
+    }
+
+    public void Output()
+    {
+      System.out.print("Ho Ten : " + this.getName()+", Tuoi : "+this.getAge()+", Dia chi : "+this.getAddress()) ;
     }
 
     @Override
@@ -42,7 +69,10 @@ public class Student {
 
     public static void main(String[] args) {
         Student st1 = new Student("Trinh", 18, "Quang Nam");
-
+        Student st2 = new Student();
         System.out.println(st1); // This will call the toString() method
+        st2.Input();
+        st2.Output();
+
     }
 }
